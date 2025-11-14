7d41b24d620a0266300b1e30033561888117c99c
Samuel <samawyck@gmail.com>
2025-11-14 16:22:07 -0600

    Add guard to save on keyboard interrupt

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from palindromicNumberFinder import PalindromeFinder
 
 def main():
     palindrome_finder = PalindromeFinder()
-    palindrome_finder.start()
+    try:
+        palindrome_finder.start()
+    except KeyboardInterrupt:
+        print("Shutting down gracefully...")
+        palindrome_finder.save_found_palindromes()
+        print("Done")
 
 
 
