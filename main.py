0dff5597c480c2e2478ba5c92786ff25d19b6edb
Samuel <samawyck@gmail.com>
2025-11-14 16:11:02 -0600

    Add main file as top level file

---
+++ main.py
@@ -0,0 +1,13 @@
+from palindromicNumberFinder import PalindromeFinder
+
+
+
+def main():
+    palindrome_finder = PalindromeFinder()
+    palindrome_finder.start()
+
+
+
+
+if __name__ == "__main__":
+    main()
