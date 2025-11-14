6e893181b7b1dc650c94e4ebf839454729d52c41
Samuel <samawyck@gmail.com>
2025-11-14 17:27:34 -0600

    Cover all exceptions in catch

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     palindrome_finder = PalindromeFinder()
     try:
         palindrome_finder.start()
-    except KeyboardInterrupt:
+    except:
         print("Shutting down gracefully...")
         palindrome_finder.save_found_palindromes()
         print("Done")
